5db8ae97b19710bddf4bcbdd76d9d181771815e1
Eli Huebner <elimhuebner@gmail.com>
2020-09-15 16:12:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,11 @@
 # code here!
 
 class School
+  attr_reader :roster
   
   def initialize(name)
     @name = name
     @roster = []
-  end
-  
-  def roster
-    @roster
   end
   
   def add_student(name, grade)
