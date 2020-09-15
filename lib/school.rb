6291c7e07f04c78233bada2c0b937ca59f2ce695
Eli Huebner <elimhuebner@gmail.com>
2020-09-15 15:52:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,25 +2,25 @@
 
 class School
   
-  @@roster = {}
+  @roster = {}
   
   def initialize(name)
     @name = name
   end
   
   def roster
-    @@roster
+    @roster
   end
   
   def add_student(name, grade)
-    if !@@roster[grade]
-      @@roster[grade] = []
+    if !@roster[grade]
+      @roster[grade] = []
     end
-    @@roster[grade] << name
+    @roster[grade] << name
   end
   
   def grade(grade)
-    @@roster[grade]
+    @roster[grade]
   end
   
   def sort
