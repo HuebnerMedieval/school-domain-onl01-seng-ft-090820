8b278722ecf5e6bcef21dd45325e72ac47790549
Eli Huebner <elimhuebner@gmail.com>
2020-09-15 14:34:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+
+class School
+  
+  ROSTER = {}
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    ROSTER
+  end
+  
+  def add_student(name, grade)
+    if !ROSTER[grade]
+      ROSTER[grade] = []
+    end
+    ROSTER[grade] << name
+  end
+  
+end
